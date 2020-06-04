8a5806f507efe8ee2cc51d533ddca091b84eb113
Karina Ponte <59859204+karinaponte@users.noreply.github.com>
2020-06-04 04:28:55 -0300

    Inclusão de arquivos referentes à aula 03

--- js/create-point.js
+++ js/create-point.js
@@ -24,12 +24,15 @@ function getCities(event) {
     
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
+    citySelect.innerHTML = "<option value>Selecione a cidade</option>";
+    citySelect.disabled = true;
+
     fetch(url)
             .then( res => res.json())
             .then( cities => {
-
+                
                 for(const city of cities) {
-                    citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+                    citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
             }
 
             citySelect.disabled = false;
@@ -38,3 +41,51 @@ function getCities(event) {
 
 document.querySelector("select[name=uf]")
         .addEventListener("change", getCities);
+
+// Itens de coleta
+
+// pegar todos os li's
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem);
+}
+
+const collectedItems = document.querySelector("input[name=items");
+
+let selectedItems = [];
+
+function handleSelectedItem(event) {
+    
+    const itemLi = event.target;
+
+    // adicionar ou remover uma classe com JS
+    itemLi.classList.toggle("selected");
+
+    const itemId = itemLi.dataset.id;
+
+    /* verificar se existem itens selecionados, se sim
+    pegar os itens selecionados */
+    const alreadySelected = selectedItems.findIndex(item => {
+        const itemFound = item == itemId; // será true ou false
+        return itemFound;
+    });
+
+    // se já estiver selecionado tirar da seleção
+    if(alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId;
+            return itemIsDifferent;        
+        });
+
+        selectedItems = filteredItems;
+    } else {    
+    // se não estiver selecionado, adicionar à seleção
+    selectedItems.push(itemId);
+    }   
+    
+    // atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems;
+
+}
+
